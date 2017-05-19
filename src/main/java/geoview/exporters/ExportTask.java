6aa61bcfeacbedbe0fb2fb86d4d23c7a71574e92
msj1294 <msj1294@gmail.com>
2017-05-18 20:41:22 -0700

    Revert More Unecessary Changes
    Ooops #2

--- src/main/java/geoview/exporters/ExportTask.java
+++ src/main/java/geoview/exporters/ExportTask.java
@@ -18,48 +18,48 @@ import com.itextpdf.layout.element.Table;
 import javafx.concurrent.Task;
 
 public class ExportTask extends Task<Void> {
-	
-	private ArrayList<Map<String, Object>> attrColl;
-	
-	//private LocalDateTime currentTime = LocalDateTime.now();
-	
-	private final String pdfPath = "./src/main/resources/export1.pdf";
-/*	private final String pdfPath = "./src/main/resources/export-" + currentTime.toLocalDate() + "@" + currentTime.getHour() + "-" +
-			currentTime.getMinute() + ".pdf";*/
-	
-	public ExportTask(ArrayList<Map<String, Object>> newColl) {
-		attrColl = newColl;
-	}
-
-	@Override
-	protected Void call() throws IOException {
-		File exportFile = new File(pdfPath);
-		exportFile.getParentFile().mkdirs();
-		manipulatePDF(pdfPath);
-		Desktop.getDesktop().open(exportFile);
-		return null;
-	}
-	
-	private void manipulatePDF(String dest) throws FileNotFoundException {
-	PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
-	Document doc = new Document(pdfDoc);
-		
-	String[] fields = { "SEWER_ID", "SCI", "AGE", "LENGTH", "PIPE_MATERIAL" };
-	float[] fieldWidth = { 1, 2, 2, 2, 2 };
-		
-	Table table = new Table(fieldWidth);
-	table.setWidthPercent(100);
-	
-	for(int i = 0; i < fields.length; i++) {
-		table.addCell(new Cell().add(new Paragraph(fields[i])));
-	}
-	for(Map<String, Object> attr : attrColl) {
-		for(int i = 0; i < fields.length; i++) {
-			table.addCell(new Cell().add(new Paragraph(attr.get(fields[i]).toString())));
-		}
-	}
-    doc.add(table);
-	doc.close();
-}
-
+    
+    private ArrayList<Map<String, Object>> attrColl;
+    
+    //private LocalDateTime currentTime = LocalDateTime.now();
+    
+    private final String pdfPath = "./src/main/resources/export1.pdf";
+    /*	private final String pdfPath = "./src/main/resources/export-" + currentTime.toLocalDate() + "@" + currentTime.getHour() + "-" +
+     currentTime.getMinute() + ".pdf";*/
+    
+    public ExportTask(ArrayList<Map<String, Object>> newColl) {
+        attrColl = newColl;
+    }
+    
+    @Override
+    protected Void call() throws IOException {
+        File exportFile = new File(pdfPath);
+        exportFile.getParentFile().mkdirs();
+        manipulatePDF(pdfPath);
+        Desktop.getDesktop().open(exportFile);
+        return null;
+    }
+    
+    private void manipulatePDF(String dest) throws FileNotFoundException {
+        PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
+        Document doc = new Document(pdfDoc);
+        
+        String[] fields = { "SEWER_ID", "SCI", "AGE", "LENGTH", "PIPE_MATERIAL" };
+        float[] fieldWidth = { 1, 2, 2, 2, 2 };
+        
+        Table table = new Table(fieldWidth);
+        table.setWidthPercent(100);
+        
+        for(int i = 0; i < fields.length; i++) {
+            table.addCell(new Cell().add(new Paragraph(fields[i])));
+        }
+        for(Map<String, Object> attr : attrColl) {
+            for(int i = 0; i < fields.length; i++) {
+                table.addCell(new Cell().add(new Paragraph(attr.get(fields[i]).toString())));
+            }
+        }
+        doc.add(table);
+        doc.close();
+    }
+    
 }

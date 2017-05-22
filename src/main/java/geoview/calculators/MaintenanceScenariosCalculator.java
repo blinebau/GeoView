e9374d3befe4b3102d272a38098d55a23087adb2
msj1294 <msj1294@gmail.com>
2017-05-22 13:54:06 -0700

    Maintenance Scenarios Calculator
    Finished Maintenance Scenarios Calculator. Need to resolve invalid
    inputs before testing.

--- src/main/java/geoview/calculators/MaintenanceScenariosCalculator.java
+++ src/main/java/geoview/calculators/MaintenanceScenariosCalculator.java
@@ -1,37 +1,98 @@
 package geoview.calculators;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 
 
 public class MaintenanceScenariosCalculator {
-    //Make Pipe material a dropdown with options. //Include these options in schema check //Could make this into a map.
-    //clay, iron, plastic, orangeburg. (12 inch length, 12 inch radius)
-    private static double[] pipeMaterialCosts = {10.0, 10.0, 10.0, 10.0};
+    //Avg Cost Per Foot.
+    private static double sameSizeReplacementAvgCost = 220.0;
+    private static double upSizeReplacementAvgCost = 250.0;
+    private static double fullTrenchlessRehabAvgCost = 150.0;
+    private static double partialTrenchlessRehabAvgCost = 100.0;
     
-    //per foot?
-    private static double pipeReplacementAvgCost = 15.0;
-    private static double trenchlessRehabAvgCost = 30.0;
+    //Material cost (by weight, with clay being average).
+    private static double clayPipeCost = 1.0;
+    private static double ironPipeCost = 2.0;
+    private static double plasticPipeCost = 0.7;
+    private static double orangeburgPipeCost = 0.6;
+    
+    //Average depth
+    private static double averageDepth = 10.0;
+    private static double averageRadius = 50.0;
+    
     
     //For now this is implemented by calculating cost for the highest SCI pipes.
     //The highest SCI pipes will be returned, until the budget is filled.
     //If there are no high SCI pipes that fit within the budget, lower SCI pipes are considered.
-    public static List<Map<String, Object>> calculateMaintenanceScenarios(int year, String maintenanceType, int budget, List<Map<String, Object>> features){
-        int calculatedCost;
+    public static List<Map<String, Object>> calculateMaintenanceScenarios(int year, String maintenanceType, double budget, List<Map<String, Object>> features){
+        double calculatedCost = 0.0;
+        List<Map<String,Object>> budgetedFeatures = new ArrayList<>();
+        
         for(Map<String, Object> feature : features){
-            calculatedCost = calculateFeatureCost(maintenanceType, feature);
+            double featureCost = calculateFeatureCost(maintenanceType, feature);
+            if((calculatedCost + featureCost) <= budget){
+                calculatedCost += featureCost;
+                budgetedFeatures.add(feature);
+            }
         }
-        return features;
+        return budgetedFeatures;
     }
     
     //For now since we are treating all of these values as
-    private static int calculateFeatureCost(String maintenanceType, Map<String, Object> feature){
-        int length = Integer.parseInt(feature.get("LENGTH").toString());
-        int depth = Integer.parseInt(feature.get("DEPTH").toString());
+    private static double calculateFeatureCost(String maintenanceType, Map<String, Object> feature){
+        double length = Integer.parseInt(feature.get("LENGTH").toString());
+        double depth = Integer.parseInt(feature.get("DEPTH").toString());
         String pipeMaterial = feature.get("PIPE_MATERIAL").toString();
-        int radius = Integer.parseInt(feature.get("RADIUS").toString());
+        double radius = Integer.parseInt(feature.get("RADIUS").toString());
         
-        return 0;
+        double costPerFoot = length * getPipeTypeValue(pipeMaterial) * getReplacementTypeValue(maintenanceType) * getDepthWeight(depth) * getRadiusWeight(radius);
+        
+        return costPerFoot;
+    }
+    
+    private static double getPipeTypeValue(String pipeMaterial){
+        switch(pipeMaterial){
+            case "CLAY":
+                return clayPipeCost;
+            case "IRON":
+                return ironPipeCost;
+            case "PLASTIC":
+                return plasticPipeCost;
+            case "ORANGEBURG":
+                return orangeburgPipeCost;
+            default:
+                return 1.0;
+        }
+    }
+    
+    private static double getReplacementTypeValue(String maintenanceType){
+        switch(maintenanceType){
+            case "Same-Size Replacement":
+                return sameSizeReplacementAvgCost;
+            case "Up-Size Replacement":
+                return upSizeReplacementAvgCost;
+            case "Full Trench Replacement":
+                return fullTrenchlessRehabAvgCost;
+            case "Partial Trench Replacement":
+                return partialTrenchlessRehabAvgCost;
+            default:
+                return 0.0;
+                //Throw new exception if user does not enter value? Or disallow empty values in GUI controllers.
+        }
+    }
+    
+    private static double getDepthWeight(double depth){
+        double returnWeight = 1.0;
+        double depthModifier = depth - averageDepth;
+        return Math.max(returnWeight + (depthModifier/10.0), 0.8);   
+    }
+    
+    private static double getRadiusWeight(double radius){
+        double returnWeight = 1.0;
+        double radiusModifier = radius - averageRadius;
+        return Math.max(returnWeight + (radiusModifier/30.0), 0.3);   
     }
     
 }
